0496d14ad1ab9f78630b3e920a5d7d2b917feea6
hennapaul41-hue <hennapaul41@gmail.com>
2026-04-23 16:21:22 +1300

    WIP: local changes before switching

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,17 @@
 import 'package:flutter/material.dart';
-import 'services/local_storage.dart';
-import 'routes/app_pages.dart';
 import 'routes/app_routes.dart';
-import 'views/home_page.dart';
-import 'views/welcome_page.dart';
+import 'routes/app_pages.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  Future<bool> checkLogin() async {
-    return await LocalStorage().getLogin();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      // ✅ use AppPages
+      initialRoute: AppRoutes.welcome,
       routes: AppPages.routes,
-
-      home: FutureBuilder<bool>(
-        future: checkLogin(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-
-          return snapshot.data == true ? const HomePage() : const WelcomePage();
-        },
-      ),
     );
   }
 }
--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import '../views/home_page.dart';
-import '../views/login_page.dart';
 import '../views/welcome_page.dart';
+import '../views/home_page.dart';
+import '../views/settings_view.dart';
 import 'app_routes.dart';
 
 class AppPages {
   static Map<String, WidgetBuilder> routes = {
-    AppRoutes.home: (context) => const HomePage(),
-    AppRoutes.login: (context) => const LoginPage(),
     AppRoutes.welcome: (context) => const WelcomePage(),
+    AppRoutes.home: (context) => const HomePage(),
+    AppRoutes.settings: (context) => const SettingsView(),
   };
 }
